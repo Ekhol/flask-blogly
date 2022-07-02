5ece38aebb40fc695dd55a9c8b856a0014d3132c
Nathan <frost.nathan1995@gmail.com>
2022-07-01 21:30:56 -0400

    finished part 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from crypt import methods
 from flask import Flask, request, redirect, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 from importlib_metadata import method_cache
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 app.debug = True
@@ -21,7 +21,13 @@ db.create_all()
 
 @app.route('/')
 def home():
-    return redirect("/users")
+    posts = Post.query.order_by(Post.created_at.desc()).limit(5).all()
+    return render_template("posts/homepage.html", posts=posts)
+
+
+@app.errorhandler(404)
+def page_not_found():
+    return render_template('404.html'), 404
 
 
 @app.route('/users')
@@ -57,13 +63,13 @@ def show_user(user_id):
     return render_template("users/details.html", user=user)
 
 
-@app.route('/user/<int:user_id>/edit')
+@app.route('/users/<int:user_id>/edit', methods=["GET"])
 def edit_user(user_id):
     user = User.query.get_or_404(user_id)
     return render_template("users/edit.html", user=user)
 
 
-@app.route('/user/<int:user_id>/edit', methods=["POST"])
+@app.route('/users/<int:user_id>/edit', methods=["POST"])
 def update_edit_user(user_id):
     user = User.query.get_or_404(user_id)
     user.first_name = request.form['first_name']
@@ -83,3 +89,57 @@ def delete_user(user_id):
     db.session.commit()
 
     return redirect("/users")
+
+#####################################PART TWO#########################################
+
+
+@app.route('/users/<int:user_id>/posts/new', methods=["GET"])
+def new_post_form(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('posts/new.html', user=user)
+
+
+@app.route('/users/<int:user_id>/posts/new', methods=["POST"])
+def new_posts(user_id):
+    user = User.query.get_or_404(user_id)
+    new_post = Post(title=request.form['title'],
+                    content=request.form['content'], user=user)
+
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect(f"/users/{user_id}")
+
+
+@app.route('/posts/<int:post_id>')
+def show_posts(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('posts/display.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit')
+def edit_posts(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('posts/edit.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit', methods=["POST"])
+def edit_existing_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(f"/users/{post.user_id}")
+
+
+@app.route('/posts/<int:post_id>/delete', methods=["POST"])
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+
+    db.session.delete(post)
+    db.session.commit()
+
+    return redirect(f"/users/{post.user_id}")
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 db = SQLAlchemy()
 
@@ -19,6 +20,28 @@ class User(db.Model):
     last_name = db.Column(db.Text, nullable=False)
     image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE)
 
+    posts = db.relationship("Post", backref="user",
+                            cascade="all, delete-orphan")
+
     @property
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class Post(db.Model):
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.datetime.now
+    )
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    @property
+    def clean_date(self):
+        return self.created_at.strftime("%A %B %-d %Y")
